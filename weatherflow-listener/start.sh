63a1f416fbb9ebb1b534e4f190de8a397744b8e1
Dave Schmid <dave@pulpfree.org>
2021-04-07 00:49:08 -0500

    Delete start.sh

--- weatherflow-listener/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/stdbuf -oL /usr/bin/python /weatherflow-listener/weatherflow-listener.py | /usr/bin/promtail --stdin --client.url http://loki:3100/loki/api/v1/push --client.external-labels=app=weatherflow,hostname=weatherflow
