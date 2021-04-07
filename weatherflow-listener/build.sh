d5fb2b7be45209c860dfe6b2f181e1a110d062ee
Dave Schmid <dave@pulpfree.org>
2021-04-07 00:49:04 -0500

    Delete build.sh

--- weatherflow-listener/build.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t lux4rd0/weatherflow-listener:latest -t docker01.tylephony.com:5000/lux4rd0/weatherflow-listener:latest
-docker push docker01.tylephony.com:5000/lux4rd0/weatherflow-listener:latest
