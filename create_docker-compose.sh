edd9db7f41e8090964dbff932495602fc634ed43
Dave Schmid <dave@pulpfree.org>
2021-04-15 00:28:51 -0500

    Update create_docker-compose.sh

--- create_docker-compose.sh
+++ create_docker-compose.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-remote_collector_token=$1
+##
+## WeatherFlow Collector AIO - docker-compose.yml generator
+##
 
-if [ -z "$remote_collector_token" ]
+token=$1
+
+if [ -z "$token" ]
 
 then
       echo "Missing authentication token. Please provide your token as a command parameter."
 else
 
-url_stations="https://swd.weatherflow.com/swd/rest/stations?token=${remote_collector_token}"
+url_stations="https://swd.weatherflow.com/swd/rest/stations?token=${token}"
 
 #echo "url_stations=${url_stations}"
 
-url_forecasts="https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${remote_collector_station_id}&token=${remote_collector_token}"
+url_forecasts="https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${station_id}&token=${token}"
 
 #echo "url_forecasts=${url_forecasts}"
 
-url_observations="https://swd.weatherflow.com/swd/rest/observations/station/${remote_collector_station_id}?token=${remote_collector_token}"
+url_observations="https://swd.weatherflow.com/swd/rest/observations/station/${station_id}?token=${token}"
 
 #echo "url_observations=${url_observations}"
+
 
 response_url_stations=$(curl -si -w "\n%{size_header},%{size_download}" "${url_stations}")
 
 response_url_forecasts=$(curl -si -w "\n%{size_header},%{size_download}" "${url_forecasts}")
 
 response_url_observations=$(curl -si -w "\n%{size_header},%{size_download}" "${url_observations}")
+
+
 
 # Extract the response header size
 header_size_stations=$(sed -n '$ s/^\([0-9]*\),.*$/\1/ p' <<< "${response_url_stations}")
@@ -37,6 +44,7 @@ headers_station="${response_url_stations:0:${header_size_stations}}"
 
 # Extract the response body
 body_station="${response_url_stations:${header_size_stations}:${body_size_stations}}"
+
 
 #echo "URL Results"
 
@@ -58,22 +66,41 @@ for station_number in $(seq 0 $number_of_stations_minus_one) ; do
 
 #echo "Station Number Loop: $station_number"
 
-station_name[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].name | sed 's/ /\_/g' | sed 's/.*/\L&/' | sed 's|[<>,]||g')
-station_id[$station_number]=$(echo "${body_station}" | jq .stations[$station_number].station_id)
-device_id[$station_number]=$(echo "${body_station}" | jq .stations[$station_number].devices[1].device_id)
+
+
+
+
+
+timezone[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].timezone)
+latitude[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].latitude)
+longitude[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].longitude)
+elevation[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].station_meta.elevation)
+public_name[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].public_name)
+station_name[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].name)
+
+station_name_dc[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].name | sed 's/ /\_/g' | sed 's/.*/\L&/' | sed 's|[<>,]||g')
+station_id[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].station_id)
+device_id[$station_number]=$(echo "${body_station}" | jq -r .stations[$station_number].devices[1].device_id)
+hub_sn[$station_number]=$(echo "${body_station}" |jq -r .stations[$station_number].devices[0].serial_number)
+
 
 done
+
 
 FILE="${PWD}/docker-compose.yml"
 if test -f "$FILE"; then
 
-existing_file_timestamp=$(date -r $FILE "+%Y%m%d-%H%M%S")
+
+
+existing_file_timestamp=$(date -r "$FILE" "+%Y%m%d-%H%M%S")
 
 echo "Existing docker-compose.yml file found. Backup up file to $FILE.${existing_file_timestamp}"
 
-mv $FILE $FILE.${existing_file_timestamp}
+mv "$FILE" "$FILE"."${existing_file_timestamp}"
 
 fi
+
+
 
 ##
 ## Print docker-compose.yml file
@@ -82,6 +109,7 @@ fi
 ##
 ## Only one UDP per location
 ##
+
 
 echo "
 
@@ -96,7 +124,7 @@ services:
       GF_AUTH_BASIC_ENABLED: \"true\"
       GF_AUTH_DISABLE_LOGIN_FORM: \"false\"
       GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH: /var/lib/grafana/dashboards/weatherflow_collector/weatherflow_collector-overview-influxdb.json
-    image: grafana/grafana:7.5.3
+    image: grafana/grafana:7.5.4
     networks:
       wxfdashboardsaio: null
     ports:
@@ -127,26 +155,8 @@ services:
     volumes:
     - ./data/influxdb:/var/lib/influxdb:rw
 
-  weatherflow-collector-${station_name[$station_number]}-local-udp:
-    container_name: weatherflow-collector-${station_name[$station_number]}-local-udp
-    environment:
-      WEATHERFLOW_COLLECTOR_BACKEND_TYPE: influxdb
-      WEATHERFLOW_COLLECTOR_COLLECTOR_TYPE: local-udp
-      WEATHERFLOW_COLLECTOR_DEBUG: \"false\"
-      WEATHERFLOW_COLLECTOR_INFLUXDB_PASSWORD: x8egQTrf4bGl8Cs3XGyF1yE0b06pfgJe
-      WEATHERFLOW_COLLECTOR_INFLUXDB_URL: http://wxfdashboardsaio_influxdb:8086/write?db=weatherflow
-      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: weatherflow
-    image: lux4rd0/weatherflow-collector:latest
-    ports:
-    - protocol: udp
-      published: 50222
-      target: 50222
-    restart: always
-    networks:
-      wxfdashboardsaio: null
-    restart: always
-    depends_on:
-      - \"wxfdashboardsaio_influxdb\"" > docker-compose.yml
+" > docker-compose.yml
+
 
 ## Loop through each device
 
@@ -157,18 +167,60 @@ echo "Station ID: ${station_id[$station_number]}"
 echo "Device ID: ${device_id[$station_number]}"
 
 echo "
-  weatherflow-collector-${station_name[$station_number]}-remote-forecast:
-    container_name: weatherflow-collector-${station_name[$station_number]}-remote-forecast
+
+  weatherflow-collector-${station_name_dc[$station_number]}-local-udp:
+    container_name: weatherflow-collector-${station_name_dc[$station_number]}-local-udp
+    environment:
+      WEATHERFLOW_COLLECTOR_BACKEND_TYPE: influxdb
+      WEATHERFLOW_COLLECTOR_COLLECTOR_TYPE: local-udp
+      WEATHERFLOW_COLLECTOR_DEBUG: \"false\"
+      WEATHERFLOW_COLLECTOR_ELEVATION: ${elevation[$station_number]}
+      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_HUB_SN: ${hub_sn[$station_number]}
+      WEATHERFLOW_COLLECTOR_INFLUXDB_PASSWORD: x8egQTrf4bGl8Cs3XGyF1yE0b06pfgJe
+      WEATHERFLOW_COLLECTOR_INFLUXDB_URL: http://wxfdashboardsaio_influxdb:8086/write?db=weatherflow
+      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: weatherflow
+      WEATHERFLOW_COLLECTOR_LATITUDE: ${latitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_LONGITUDE: ${longitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_PUBLIC_NAME: ${public_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_ID: ${station_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_NAME: ${station_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_TIMEZONE: ${timezone[$station_number]}
+      WEATHERFLOW_COLLECTOR_TOKEN: ${token}
+    image: lux4rd0/weatherflow-collector:latest
+    ports:
+    - protocol: udp
+      published: 50222
+      target: 50222
+    restart: always
+    networks:
+      wxfdashboardsaio: null
+    restart: always
+    depends_on:
+      - \"wxfdashboardsaio_influxdb\"
+
+  weatherflow-collector-${station_name_dc[$station_number]}-remote-forecast:
+    container_name: weatherflow-collector-${station_name_dc[$station_number]}-remote-forecast
     environment:
       WEATHERFLOW_COLLECTOR_BACKEND_TYPE: influxdb
       WEATHERFLOW_COLLECTOR_COLLECTOR_TYPE: remote-forecast
       WEATHERFLOW_COLLECTOR_DEBUG: \"false\"
+      WEATHERFLOW_COLLECTOR_DEVICE_ID: ${device_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_ELEVATION: ${elevation[$station_number]}
+      WEATHERFLOW_COLLECTOR_FORECAST_INTERVAL: 60
+      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_HUB_SN: ${hub_sn[$station_number]}
       WEATHERFLOW_COLLECTOR_INFLUXDB_PASSWORD: x8egQTrf4bGl8Cs3XGyF1yE0b06pfgJe
       WEATHERFLOW_COLLECTOR_INFLUXDB_URL: http://wxfdashboardsaio_influxdb:8086/write?db=weatherflow
-      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: influxdb
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_STATION_ID: ${station_id[$station_number]}
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_TOKEN: ${remote_collector_token}
-      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: weatherflow
+      WEATHERFLOW_COLLECTOR_LATITUDE: ${latitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_LONGITUDE: ${longitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_PUBLIC_NAME: ${public_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_REST_INTERVAL: 60
+      WEATHERFLOW_COLLECTOR_STATION_ID: ${station_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_NAME: ${station_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_TIMEZONE: ${timezone[$station_number]}
+      WEATHERFLOW_COLLECTOR_TOKEN: ${token}
     image: lux4rd0/weatherflow-collector:latest
     networks:
       wxfdashboardsaio: null
@@ -176,18 +228,26 @@ echo "
     depends_on:
       - \"wxfdashboardsaio_influxdb\"
 
-  weatherflow-collector-${station_name[$station_number]}-remote-rest:
-    container_name: weatherflow-collector-${station_name[$station_number]}-remote-rest
+  weatherflow-collector-${station_name_dc[$station_number]}-remote-rest:
+    container_name: weatherflow-collector-${station_name_dc[$station_number]}-remote-rest
     environment:
       WEATHERFLOW_COLLECTOR_BACKEND_TYPE: influxdb
       WEATHERFLOW_COLLECTOR_COLLECTOR_TYPE: remote-rest
       WEATHERFLOW_COLLECTOR_DEBUG: \"false\"
+      WEATHERFLOW_COLLECTOR_DEVICE_ID: ${device_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_ELEVATION: ${elevation[$station_number]}
+      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_HUB_SN: ${hub_sn[$station_number]}
       WEATHERFLOW_COLLECTOR_INFLUXDB_PASSWORD: x8egQTrf4bGl8Cs3XGyF1yE0b06pfgJe
       WEATHERFLOW_COLLECTOR_INFLUXDB_URL: http://wxfdashboardsaio_influxdb:8086/write?db=weatherflow
-      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: influxdb
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_STATION_ID: ${station_id[$station_number]}
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_TOKEN: ${remote_collector_token}
-      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: weatherflow
+      WEATHERFLOW_COLLECTOR_LATITUDE: ${latitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_LONGITUDE: ${longitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_PUBLIC_NAME: ${public_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_ID: ${station_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_NAME: ${station_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_TIMEZONE: ${timezone[$station_number]}
+      WEATHERFLOW_COLLECTOR_TOKEN: ${token}
     image: lux4rd0/weatherflow-collector:latest
     networks:
       wxfdashboardsaio: null
@@ -195,19 +255,26 @@ echo "
     depends_on:
       - \"wxfdashboardsaio_influxdb\"
 
-  weatherflow-collector-${station_name[$station_number]}-remote-socket:
-    container_name: weatherflow-collector-${station_name[$station_number]}-remote-socket
+  weatherflow-collector-${station_name_dc[$station_number]}-remote-socket:
+    container_name: weatherflow-collector-${station_name_dc[$station_number]}-remote-socket
     environment:
       WEATHERFLOW_COLLECTOR_BACKEND_TYPE: influxdb
       WEATHERFLOW_COLLECTOR_COLLECTOR_TYPE: remote-socket
       WEATHERFLOW_COLLECTOR_DEBUG: \"false\"
+      WEATHERFLOW_COLLECTOR_DEVICE_ID: ${device_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_ELEVATION: ${elevation[$station_number]}
+      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_HUB_SN: ${hub_sn[$station_number]}
       WEATHERFLOW_COLLECTOR_INFLUXDB_PASSWORD: x8egQTrf4bGl8Cs3XGyF1yE0b06pfgJe
       WEATHERFLOW_COLLECTOR_INFLUXDB_URL: http://wxfdashboardsaio_influxdb:8086/write?db=weatherflow
-      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: influxdb
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_DEVICE_ID: ${device_id[$station_number]}
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_STATION_ID: ${station_id[$station_number]}
-      WEATHERFLOW_COLLECTOR_REMOTE_COLLECTOR_TOKEN: ${remote_collector_token}
-      WEATHERFLOW_COLLECTOR_HOST_HOSTNAME: $(hostname)
+      WEATHERFLOW_COLLECTOR_INFLUXDB_USERNAME: weatherflow
+      WEATHERFLOW_COLLECTOR_LATITUDE: ${latitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_LONGITUDE: ${longitude[$station_number]}
+      WEATHERFLOW_COLLECTOR_PUBLIC_NAME: ${public_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_ID: ${station_id[$station_number]}
+      WEATHERFLOW_COLLECTOR_STATION_NAME: ${station_name[$station_number]}
+      WEATHERFLOW_COLLECTOR_TIMEZONE: ${timezone[$station_number]}
+      WEATHERFLOW_COLLECTOR_TOKEN: ${token}
     image: lux4rd0/weatherflow-collector:latest
     networks:
       wxfdashboardsaio: null
