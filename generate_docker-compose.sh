c9a134a7875561df99d13bbef21ec386d747d8a1
Dave Schmid <dave@pulpfree.org>
2021-05-03 09:23:45 -0500

    Update generate_docker-compose.sh

--- generate_docker-compose.sh
+++ generate_docker-compose.sh
@@ -139,7 +139,8 @@ services:
       GF_AUTH_BASIC_ENABLED: \"true\"
       GF_AUTH_DISABLE_LOGIN_FORM: \"false\"
       GF_DASHBOARDS_DEFAULT_HOME_DASHBOARD_PATH: /var/lib/grafana/dashboards/weatherflow_collector/weatherflow_collector-overview-influxdb.json
-    image: grafana/grafana:7.5.4
+      GF_INSTALL_PLUGINS: grafana-worldmap-panel
+    image: grafana/grafana:7.5.5
     networks:
       wxfdashboardsaio:
     ports:
